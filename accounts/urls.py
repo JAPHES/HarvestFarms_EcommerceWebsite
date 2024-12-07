8ebc0ad8ab9e8cda12377bc67e366c51c7b340e5
JAPHES <japhesmurithi@gmail.com>
2024-12-07 11:38:42 +0300

    added html and css elements

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),  # URL for registration
-    #path('login/', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('', views.register_view, name='register'),
 
