76cdbe238860e1a504f7e71156110b864f60d95f
JAPHES <japhesmurithi@gmail.com>
2024-12-10 16:16:02 +0300

    chages on database

--- contact/urls.py
+++ contact/urls.py
@@ -5,11 +5,6 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.contact_form_view, name='contact_form'),  # Match the `contact/` path
     path('success/', views.success_view, name='success'),
-    #path('', views.index, name= 'home'),
-   # path('contact/', views.contact_view, name='contact'),
-
-
-
-]
+    ]
 
 
