10748dc1755384880f321a8a22070c7948a6c236
JAPHES <japhesmurithi@gmail.com>
2025-01-19 00:38:39 +0300

    add whitenoise and configure static files

--- HarvestFarms_EcommerceWebsite/settings.py
+++ HarvestFarms_EcommerceWebsite/settings.py
@@ -140,6 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORANGE = 'whitenoise.storange.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
